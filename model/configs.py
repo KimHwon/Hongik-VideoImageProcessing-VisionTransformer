12fe8d75d7e369b765ce1fe511604537e7965832
KimHwon <hwon233@gmail.com>
2022-12-06 03:05:49 +0900

    Final update.

--- model/configs.py
+++ model/configs.py
@@ -1,17 +1,21 @@
 
+def select_configs(arch):
+    if arch == 'B_16':
+        return get_base_config()
+    else:
+        raise NotImplementedError(f"Unknown model '{arch}'.")
+
 def get_base_config():
     """
     ViT-B/16 Imagenet1K
     """
     return dict({
-        'config': {
-            'dim': 768,
-            'dropout_rate': 0.1,
-            'ff_dim': 3072,
-            'num_heads': 12,
-            'num_layers': 12,
-            'patches': 16,
-        },
         'image_size': 224,
+        'patch_size': 16,
+        'num_heads': 12,
+        'num_layers': 12,
+        'embed_size': 768,
+        'mlp_size': 3072,
+        'dropout_rate': 0.1,
         'num_classes': 1000
     })
